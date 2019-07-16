030150b4d146c19d273068c1fc3a51ab2d00d979
zhaorch <zhaorch@163.com>
2019-07-16 14:09:21 +0800

    swagger

--- src/main/java/com/zrc/springboottutorial/controller/HelloController.java
+++ src/main/java/com/zrc/springboottutorial/controller/HelloController.java
@@ -10,8 +10,13 @@ import com.zrc.springboottutorial.response.CommonReturnType;
 import com.zrc.springboottutorial.service.SysUserService;
 import com.zrc.springboottutorial.utils.JsonUtils;
 import com.zrc.springboottutorial.utils.RedisOperator;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Api(value = "HelloController")
 public class HelloController{
 
     @Autowired
@@ -96,5 +102,30 @@ public class HelloController{
     @RequestMapping("getResource")
     public CommonReturnType getResource(){
         return  CommonReturnType.create(resource.getName());
+    }
+
+    /**
+     * 用户登录
+     */
+    @ApiOperation(value = "登录")
+    @ApiImplicitParams({@ApiImplicitParam(name = "userName", value = "用户名", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "password", value = "密码", required = true, dataType = "String")})
+    @PostMapping("/login")
+    //@SystemControllerLog(description = "/admin/user/login")
+    public CommonReturnType login(String userName, String password){
+//        @Api： 描述 Controller
+//        @ApiIgnore： 忽略该 Controller，指不对当前类做扫描
+//        @ApiOperation： 描述 Controller类中的 method接口
+//        @ApiParam： 单个参数描述，与 @ApiImplicitParam不同的是，他是写在参数左侧的。如（ @ApiParam(name="username",value="用户名")Stringusername）
+//        @ApiModel： 描述 POJO对象
+//        @ApiProperty： 描述 POJO对象中的属性值
+//        @ApiImplicitParam： 描述单个入参信息
+//        @ApiImplicitParams： 描述多个入参信息
+//        @ApiResponse： 描述单个出参信息
+//        @ApiResponses： 描述多个出参信息
+//        @ApiError： 接口错误所返回的信息
+        //http://localhost:8090/swagger-ui.html
+
+        return CommonReturnType.create(userName);
     }
 }
