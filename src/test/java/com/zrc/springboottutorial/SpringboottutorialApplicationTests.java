dd4b2a40919a4a8a354cd26a67172340e7048b96
zhaorch <zhaorch@163.com>
2019-08-06 14:58:24 +0800

    test

--- src/test/java/com/zrc/springboottutorial/SpringboottutorialApplicationTests.java
+++ src/test/java/com/zrc/springboottutorial/SpringboottutorialApplicationTests.java
@@ -1,16 +1,46 @@
 package com.zrc.springboottutorial;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringboottutorialApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp (){
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
+    @Test
+    public void whenQuerySuccess () throws Exception{
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/helloworld2")
+                .param("name","zrc")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)).andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+
+        Assert.assertEquals("Hello zrc,zrc",result.getResponse().getContentAsString());
+    }
+    @Test
+    public void contextLoads() {
+
+    }
 }
